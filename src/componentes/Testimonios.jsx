1b0fc1cb16954d6d32cef899c722f29f92388507
PaulaMelia <progne.liber@gmail.com>
2023-03-18 18:50:21 +0100

    mejoras props

--- src/componentes/Testimonios.jsx
+++ src/componentes/Testimonios.jsx
@@ -6,7 +6,9 @@ function Testimonio(props){
     <div className='contenedor-testimonio'>
         <img className='imagen-testimonio' 
         src= {require(`../imagenes/testimonio-${props.imagen}.png`)}
-        alt='Foto de Emma'/>
+        alt={`Foto de-${props.nombre}`}
+        title={`Foto del testimonio ${props.nombre}`} 
+        />
       <div className='contenedor-textotestimonio'>
         <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
         <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong> </p>
